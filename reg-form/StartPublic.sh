807da1d147ca6f4113ae64a56541200e27450624
Joe Dillig <97898347+joe-at-torq@users.noreply.github.com>
2022-08-10 15:59:46 -0500

    adjustments

--- reg-form/StartPublic.sh
+++ reg-form/StartPublic.sh
@@ -2,7 +2,8 @@
 
 #Start Public LDS GUI
 
-export FLASK_APP=${PWD}/Public/reg-form/reg-process.py
+export FLASK_APP=${PWD}reg-process.py
 export FLASK_RUN_PORT=5000
+export FLASK_DEBUG=0
 
 flask run --host=0.0.0.0
--- reg-form/reg-process.py
+++ reg-form/reg-process.py
@@ -1,50 +1,50 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import time, subprocess, os
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"]) #Front page load
 def form():
-	return render_template('reg-form.html')
+        return render_template('reg-form.html')
 
 @app.route('/processform', methods=["POST"]) #On submit -> Process data
 def processform():
-	if request.method == 'POST': 
-		name = request.form.get('name')
-		email = request.form['email']
-		countrycode = request.form['countrycode']
-		phone = request.form['phone']
-		selectedclass = request.form['selectedclass']
-		user_ip = request.environ['REMOTE_ADDR']
-		lab_duration = request.form['labduration']
+        if request.method == 'POST':
+                name = request.form.get('name')
+                email = request.form['email']
+                #countrycode = request.form['countrycode']
+                #phone = request.form['phone']
+                selectedclass = request.form['selectedclass']
+                user_ip = request.environ['REMOTE_ADDR']
+                lab_duration = request.form['labduration']
 
-		#Adjust Parameters
-		firstname,lastname=name.split(" ")
-		username=firstname[0]+lastname
-		fullphone='+'+countrycode+phone
+                #Adjust Parameters
+                firstname,lastname=name.split(" ")
+                username=firstname[0]+lastname
+                #fullphone='+'+countrycode+phone
 
-		#Time Stamp
-		localtime = time.asctime( time.localtime(time.time()) )
+                #Time Stamp
+                localtime = time.asctime( time.localtime(time.time()) )
 
-		#Write Log Entry
-		f = open("lds.log","a")
-		f.write("#######################################\n")
-		f.write("Name: "+name+"\n")
-		f.write("Username: "+username+"\n")
-		f.write("Email: "+email+"\n")
-		f.write("Phone Number: "+fullphone+"\n")
-		f.write("Selected Class: "+selectedclass+"\n")
-		f.write("Lab Duration: "+lab_duration+"\n")
-		f.write("Source IP: "+user_ip+"\n")
-		f.write("Request Time: "+str(localtime)+"\n")
+                #Write Log Entry
+                f = open("lds.log","a")
+                f.write("#######################################\n")
+                f.write("Name: "+name+"\n")
+                f.write("Username: "+username+"\n")
+                f.write("Email: "+email+"\n")
+                #f.write("Phone Number: "+fullphone+"\n")
+                f.write("Selected Class: "+selectedclass+"\n")
+                f.write("Lab Duration: "+lab_duration+"\n")
+                f.write("Source IP: "+user_ip+"\n")
+                f.write("Request Time: "+str(localtime)+"\n")
 
-		#Send data to lds_deploy.py
-		#print ('python lds_deploy.py '+str(selectedclass)+' '+str(username)+' '+str(fullphone)+' '+str(email)+' '+str(lab_duration))
-		subprocess.Popen('python lds_deploy.py '+str(selectedclass)+' '+str(username)+' '+str(fullphone)+' '+str(email)+' '+str(lab_duration)+' &', cwd=os.getcwd(), shell=True)
-		#Example:  python lds_deploy.py Automation Training JDillig +18172695082 comptech.joe@gmail.com 2
+                #Send data to lds_deploy.py
+                #print ('python lds_deploy.py '+str(selectedclass)+' '+str(username)+' '+str(fullphone)+' '+str(email)+' '+str(lab_duration))
+                #subprocess.Popen('python lds_deploy.py '+str(selectedclass)+' '+str(username)+' '+str(fullphone)+' '+str(email)+' '+str(lab_duration)+' &', cwd=os.getcwd(), shell=True)
 
-		
-		f.write("#######################################\n")
+
+
+                f.write("#######################################\n")
                 f.close()
 
-	return render_template('reg-summary.html') #after processing data, present the summary page
+        return render_template('reg-summary.html') #after processing data, present the summary page
