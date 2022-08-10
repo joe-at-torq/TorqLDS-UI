0a26ae098e279aa7a9afdb803fa9a40c654c507d
Joe Dillig <97898347+joe-at-torq@users.noreply.github.com>
2022-08-10 15:21:09 -0500

    Update StartPublic.sh

--- StartPublic.sh
+++ StartPublic.sh
@@ -2,7 +2,7 @@
 
 #Start Public LDS GUI
 
-export FLASK_APP=/home/lds/WebGUI/Public/reg-form/reg-process.py
+export FLASK_APP=${PWD}/Public/reg-form/reg-process.py
 export FLASK_RUN_PORT=5000
 
 flask run --host=0.0.0.0
