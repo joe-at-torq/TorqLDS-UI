7722687bd806a7371aaf4464c1adbd9eaa60a6b9
Joe Dillig <97898347+joe-at-torq@users.noreply.github.com>
2022-10-03 10:46:38 -0500

    Added SSL certificates for flask run

--- reg-form/StartPublic.sh
+++ reg-form/StartPublic.sh
@@ -3,7 +3,7 @@
 #Start Public LDS GUI
 
 export FLASK_APP=${PWD}/reg-process.py
-export FLASK_RUN_PORT=8080
+export FLASK_RUN_PORT=443
 export FLASK_DEBUG=0
 
-flask run --host=0.0.0.0
+flask run --host=0.0.0.0 --cert=cert.pem --key=key.pem
